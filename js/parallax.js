c4dd91ad87d46cecf9cc9c6034d2f8f8e94fdfea
piotrgondek <piotrgondek@gmail.com>
2015-02-07 16:17:32 +0100

    Sections

--- js/parallax.js
+++ js/parallax.js
@@ -1,30 +1,25 @@
-$(document).ready(function () {
-    if ($("#js-parallax-window").length) {
-        parallax();
+(function ($) {
+    'use strict';
+
+    $.fn.parallax = function () {
+        return this.each(function () {
+            var $this = $(this);
+            var plxBackground = $this.find('.parallax-background');
+
+            var plxWindowTopToPageTop = $this.offset().top;
+            var windowTopToPageTop = $(window).scrollTop();
+            var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+            var plxSpeed = 0.35;
+
+            plxBackground.css('top', -(plxWindowTopToWindowTop * plxSpeed) + 'px');
+        });
     }
+})(jQuery);
+
+$(document).ready(function () {
+    $(".parallax-window").parallax();
 });
 
 $(window).scroll(function (e) {
-    if ($("#js-parallax-window").length) {
-        parallax();
-    }
+    $(".parallax-window").parallax();
 });
-
-function parallax() {
-    if ($("#js-parallax-window").length > 0) {
-        var plxBackground = $("#js-parallax-background");
-        var plxWindow = $("#js-parallax-window");
-
-        var plxWindowTopToPageTop = $(plxWindow).offset().top;
-        var windowTopToPageTop = $(window).scrollTop();
-        var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
-
-        var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
-        var windowInnerHeight = window.innerHeight;
-        var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
-        var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-        var plxSpeed = 0.35;
-
-        plxBackground.css('top', -(plxWindowTopToWindowTop * plxSpeed) + 'px');
-    }
-}
